c2c7911c05310eb2dce8e5b8c783d1f2c1e2d181
ÜMİT KARABACAK <umitkarabacak@koc.k12.tr>
2021-12-27 01:10:03 +0300

    - Created WebAPI in presentation layer

---
+++ src/Presentation/WebAPI/Controllers/BaseApiController.cs
@@ -0,0 +1,23 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BaseApiController : ControllerBase
+    {
+        private ISender _mediator = null!;
+        public readonly ILogger Logger; // Todo encapsulation
+
+        public BaseApiController(ILogger logger)
+        {
+            Logger = logger;
+        }
+
+        protected ISender Mediator => _mediator ??= HttpContext.RequestServices.GetRequiredService<ISender>();
+    }
+}
