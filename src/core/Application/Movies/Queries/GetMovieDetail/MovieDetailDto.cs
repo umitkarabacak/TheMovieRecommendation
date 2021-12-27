c9e7633f2ac33f2d17672080124e2a6035c532fe
ÜMİT KARABACAK <umitkarabacak@koc.k12.tr>
2021-12-28 01:36:40 +0300

    change get detail properties

--- src/core/Application/Movies/Queries/GetMovieDetail/MovieDetailDto.cs
+++ src/core/Application/Movies/Queries/GetMovieDetail/MovieDetailDto.cs
@@ -9,14 +9,18 @@ namespace Application.Movies.Queries.GetMovieDetail
     public class MovieDetailDto : IMapFrom<Movie>
     {
         public int Id { get; set; }
-
         public string Title { get; set; }
+        public string Overview { get; set; }
+        public string Original_language { get; set; }
+        public string Original_title { get; set; }
+        public string Release_date { get; set; }
+        public float Vote_average { get; set; }
+        public int Vote_count { get; set; }
+        public float Popularity { get; set; }
 
         public string Genres { get; set; }
-
-        public List<MovieVoteDetailDto> Votes { get; set; }
+        public List<MovieVoteDetailDto> LocalVotes { get; set; }
             = new List<MovieVoteDetailDto>();
-
 
         public void Mapping(Profile profile)
         {
@@ -28,7 +32,7 @@ namespace Application.Movies.Queries.GetMovieDetail
                         ? string.Join(", ", src.MovieGenres.Select(mg => mg.Genre.Name).OrderBy(gn => gn).ToList())
                         : null);
                 })
-                .ForMember(m => m.Votes, opts =>
+                .ForMember(m => m.LocalVotes, opts =>
                 {
                     opts.MapFrom(src => src.MovieVotes.Any()
                         ? src.MovieVotes.Select(mv => new MovieVoteDetailDto
